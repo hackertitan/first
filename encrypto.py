477ae33b7fb34a644b8045d5b309efc337ff295e
hackertitan <alooo131000@gmail.com>
2016-07-18 23:02:29 +0300

    Update encrypto.py

--- encrypto.py
+++ encrypto.py
@@ -94,8 +94,30 @@ sifre_bir = {
     '}': "7D",
     '~': "7E"
     }
-giris_bir = input("metniniz: ");
-sonuc_bir = "";
-for harf in giris_bir:
-    sonuc_bir = sonuc_bir + sifre_bir[harf];
-print("olusan metin : ",sonuc_bir);
+giris = input("metniniz: ");
+sonuc = "";
+for harf in giris:
+    sonuc = sonuc + sifre_bir[harf];
+mors = {
+    '0': "-----",
+    '1': ".----",
+    '2': "..---",
+    '3': "...--",
+    '4': "....-",
+    '5': ".....",
+    '6': "-....",
+    '7': "--...",
+    '8': "---..",
+    '9': "----.",
+    'A': ".-",
+    'B': "-...",
+    'C': "-.-.",
+    'D': "-..",
+    'E': '.',
+    'F': "..-.",
+    }
+giris = sonuc;
+sonuc = "";
+for harfler in giris:
+    sonuc = sonuc + mors[harfler];
+print("oluşan metin: ",sonuc);
